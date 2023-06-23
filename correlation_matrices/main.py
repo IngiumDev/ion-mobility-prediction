ee9dff7c173e7ef227911c813a83f7de37fe9105
meisfabi <mathis.fabian03@gmail.com>
2023-06-23 13:34:55 +0200

    added tenzer waters data set

--- correlation_matrices/main.py
+++ correlation_matrices/main.py
@@ -12,17 +12,24 @@ def gen_correlation_matrix(df, unusable_columns, filename):
     plt.savefig(filename)
     plt.close()
 
+
 if __name__ == '__main__':
-    raw_data = pd.read_csv(sys.argv[1])
-    df = pd.DataFrame(raw_data)
+    mb_raw_data = pd.read_csv(sys.argv[1])
+    mb_df = pd.DataFrame(mb_raw_data)
 
     # for all data
     not_used = ["Experiment", "Modifications", "Sequence", "Modified sequence", "Raw file"]
-    gen_correlation_matrix(df, not_used, "heatmap_mannbruker_all_columns.png")
+    #gen_correlation_matrix(mb_df, not_used, "heatmap_mannbruker_all_columns.png")
 
     # just ccs and ion scores
     not_used.append("Charge")
     not_used.append("Score")
     not_used.append("Intensity")
-    gen_correlation_matrix(df, not_used, "heatmap_mannbruker_simplified.png")
+    #gen_correlation_matrix(mb_df, not_used, "heatmap_mannbruker_simplified.png")
+
+    # tenzer waters
+    tw_raw_data = pd.read_csv(sys.argv[2])
+    tw_df = pd.DataFrame(tw_raw_data)
+    not_used = ["run", "sequence", "type", "modification", "rt", "LiftOffRT"]
+    gen_correlation_matrix(tw_df, not_used, "tenzer_waters")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
