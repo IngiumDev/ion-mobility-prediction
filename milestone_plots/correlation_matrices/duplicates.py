aa7f459e918d6d2bc8295cb43f326fffe0310bb9
meisfabi <mathis.fabian03@gmail.com>
2024-01-15 01:03:13 +0100

    poly features

--- milestone_plots/correlation_matrices/duplicates.py
+++ milestone_plots/correlation_matrices/duplicates.py
@@ -14,8 +14,9 @@ def duplicates_mb():
     mb_df_count = mb_df_count.rename(columns={"CCS": "Mann Bruker"})
     IQR_count = mb_df_count.quantile(0.75) - mb_df_count.quantile(0.25)
     mb_df_count.boxplot()
-    print(mb_df_count.quantile(0.75))
-    print(mb_df_count.quantile(0.25))
+    print(mb_df_count.quantile(1))
+    print(mb_df_count.quantile(0.54125))
+
     plt.ylabel("Duplicates per Sequence and Charge")
     plt.savefig("mb_count_same_seq")
     plt.close()
@@ -25,7 +26,7 @@ def duplicates_mb():
     plt.ylabel("STD of CCS Values with same Sequence and Charge")
     std_mb_df.boxplot()
     IQR_STD = std_mb_df.quantile(0.75) - std_mb_df.quantile(0.25)
-    plt.savefig("mb_std_same_seq")
+    #plt.savefig("mb_std_same_seq")
 
 
 def duplicates_tw():
@@ -33,17 +34,18 @@ def duplicates_tw():
     tw_df = pd.DataFrame(tw_raw_data)
     tw_df = tw_df[["sequence", "charge", "dt"]]
 
-    tw_df = tw_df.groupby(["sequence"])
+    tw_df = tw_df.groupby(["sequence", "charge"])
     tw_df_count = tw_df.count()
     tw_df_count["dt"] = tw_df_count["dt"] - 1
     maximum_duplicates = tw_df_count["dt"].max()
     tw_df_count = tw_df_count.rename(columns={"dt": "Tenzer Waters"})
     IQR_count = tw_df_count.quantile(0.75) - tw_df_count.quantile(0.25)
     tw_df_count.boxplot()
-    print(tw_df_count.quantile(0.75))
-    print(tw_df_count.quantile(0.25))
+    print("------------")
+    print(tw_df_count.quantile(0.74))
+    print("------------")
     plt.ylabel("Duplicates per Sequence and Charge")
-    plt.savefig("tw_count_same_seq_new")
+    plt.savefig("tw_count_same_seq_new_test")
 
     plt.close()
     std_tw_df = tw_df.transform("std")
@@ -52,7 +54,7 @@ def duplicates_tw():
     IQR_STD = std_tw_df.quantile(0.75) - std_tw_df.quantile(0.25)
    # print(IQR_STD)
 
-    plt.savefig("tw_std_same_seq")
+    #plt.savefig("tw_std_same_seq")
 
 
 if __name__ == "__main__":
